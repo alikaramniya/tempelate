cd9bab69a8208d895d54dc7143f575dc05e4e6b8
alikaram <alikaramniyabadreh98@gmail.com>
2020-12-22 12:32:27 +0330

    add all template and layout project

---
+++ panel2/CMS/include/functions.php
@@ -0,0 +1,11 @@
+<?php
+    function config(){
+        $server="localhost";
+        $user="root";
+        $spassword="";
+        $db="CMS";
+        $connect=mysqli_connect($server,$user,$spassword,$db);
+
+        return $connect;
+    }
+
